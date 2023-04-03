cd63aaa29ed6412def032e02558424f04cbbac3c
Aamir <aamirnorth@gmail.com>
2023-04-03 11:15:13 +0400

    resonsive menu

--- main.js
+++ main.js
@@ -20,19 +20,19 @@ const next = document.querySelector('.next-siema');
 prev.addEventListener('click', () => mySiema.prev());
 next.addEventListener('click', () => mySiema.next());
 
-const showMegaMenu = document.querySelectorAll(".show-menga-menu");
-const MegaMenu = document.getElementById("mega-menu");
+// const showMegaMenu = document.querySelectorAll(".show-menga-menu");
+// const MegaMenu = document.getElementById("mega-menu");
 
-// MegaMenu.classList.remove('hidden')
+// // MegaMenu.classList.remove('hidden')
 
-const toggleClassDivs = (evt) => {
-    const isEnter = evt.type === "mouseenter";
-    MegaMenu.classList[isEnter ? "remove" : "add"]("hidden");
-    evt.currentTarget.classList.remove("red");
-  };
+// const toggleClassDivs = (evt) => {
+//     const isEnter = evt.type === "mouseenter";
+//     MegaMenu.classList[isEnter ? "remove" : "add"]("hidden");
+//     evt.currentTarget.classList.remove("red");
+//   };
   
-  showMegaMenu.forEach(el => {
-    el.addEventListener("mouseenter", toggleClassDivs);
-    el.addEventListener("mouseleave", toggleClassDivs);
-  });
+//   showMegaMenu.forEach(el => {
+//     el.addEventListener("mouseenter", toggleClassDivs);
+//     el.addEventListener("mouseleave", toggleClassDivs);
+//   });
 
